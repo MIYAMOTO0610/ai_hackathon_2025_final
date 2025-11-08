6befc112b43978bf3a8ec256a1bb62cfa4e48671
miyamoto <smiyamoto.610@gmail.com>
2025-11-08 10:20:42 +0900

    chore: init

---
+++ README.md
@@ -0,0 +1,3 @@
+# ai_hackathon_2025_final
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/ai_hackathon_2025_final/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ai_hackathon_2025_final
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
